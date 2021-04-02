d013df529b899a591a706c4edc67e23b269fc986
Trotman <trotman@my.ccsu.edu>
2021-04-02 13:49:49 -0400

    expo setup

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Wellness App!</Text>
+      
+      <Button color="red" title="test" onPress={ev => {Alert.alert('You clicked me')}}/>
       <StatusBar style="auto" />
     </View>
   );
