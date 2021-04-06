f008a90f05c5a91ac0b09c00ecc33894b0d4f347
Trotman <trotman@my.ccsu.edu>
2021-04-06 13:32:56 -0400

    First commit

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
-
+import { StyleSheet, Text, View, Button, Alert, ScrollView, SafeAreaView } from 'react-native';
+import Factors from "./src/components/Factors"
 export default function App() {
   return (
+    <SafeAreaView>
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Wellness App!</Text>
-      
-      <Button color="red" title="test" onPress={ev => {Alert.alert('You clicked me')}}/>
+      {/* <Text>Wellness App!</Text> */}
+      <Factors/>
+      {/* <Button color="red" title="test" onPress={ev => {Alert.alert('You clicked me')}}/> */}
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
---
+++ src/screens/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import Factors from '../components/Factors'
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+const Main = () => {
+    return (
+        <View style={styles.container}>
+          <Factors />
+        </View>
+      );
+};
+export default Main;
